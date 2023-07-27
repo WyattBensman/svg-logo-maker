8d69191aeddc2aad95cc64ec4c2af8494acb183d
WyattBensman <wyattbensman5@gmail.com>
2023-07-27 16:36:06 -0400

    Added a logoGenerator test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateShapes = require('./generateShapes')
+const generateShapes = require('./logoGenerator')
 
 const questions = [
     {
