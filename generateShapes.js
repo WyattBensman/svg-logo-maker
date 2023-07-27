deac0dca3d1bba14866f2fbd1c6adb6cd50e4038
WyattBensman <wyattbensman5@gmail.com>
2023-07-27 15:44:49 -0400

    Added the generateShapes function

--- generateShapes.js
+++ generateShapes.js
@@ -1,6 +1,20 @@
 function generateShapes(data) {
     return `
+    <svg width="200" height="100">
+        <${shape(data.shape)} ${shapeColor(data.shapeColor)} />
+        <text x="10" y="20" font-family="Arial" font-size="16" ${textColor(data.textColor)}>${data.text}</text>
+    </svg>
     `
+}
+
+function shape(shape) {
+    if (shape === 'circle') {
+        return `circle cx="150" cy="150" r="80"`
+    } else if (shape === 'square') {
+        return `rect x="150" y="150" rx="10" ry="10" width="200" height="200"`
+    } else {
+        return `polygon points="150 45,260 245, 40 245"`
+    }
 }
 
 function shapeColor(color) {
